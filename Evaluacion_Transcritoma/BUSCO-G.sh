d9e8ae3527c9170ec1f9c0f6ad0cba9d4ee588b7
gaferguz <49653865+gaferguz@users.noreply.github.com>
2026-01-03 00:56:16 +0100

    Update BUSCO-G.sh

--- Evaluacion_Transcritoma/BUSCO-G.sh
+++ Evaluacion_Transcritoma/BUSCO-G.sh
@@ -11,14 +11,4 @@
 # module load anaconda
 # conda activate busco
 
-#busco -m genome -i FCS_GX_Cleaned_Bge2.fasta -o Busco_Bge2 -l insecta_odb12
-#busco -m genome -i HCov.scaffolds.FINAL.fasta -o Busco_Sdor -l insecta_odb12
-#busco -m genome -i NR_95_rep_seq.okay.mrna -o Busco_Transcriptome -l insecta_odb12
-#busco -m genome -i TSA_c50_t1_v2_AMPIMD.fas -o Busco_Silva -l insecta_odb12
-#busco -m genome -i RNASpades_tx.fasta -o Busco_Spa -l insecta_odb12
-#busco -m genome -i NR_95_perlib_TPM_rep_seq.okay.mrna -o Busco_perLIB_tmp_25 -l insecta_odb12
-#busco -m genome -i TPM1_10c_mrna.fasta -o Busco_TPM1_10c -l insecta_odb12
-#busco -m genome -i TPM1_20c_mrna.fasta -o Busco_TPM1_20c -l insecta_odb12
-#busco -m genome -i TPM1_subgr_mrna.fasta -o Busco_TPM1_subg -l insecta_odb12
-#busco -m genome -i TPM1_avg_mrna.fasta -o Busco_TPM1_avg -l insecta_odb12
 busco -m genome -i AMPs_Enriched_Final_TPM1_okaycull.mrna -o Busco_TPM1_enriched -l insecta_odb12
