0e9ad0705503b1b5baecd64fff69dbc6b7a3d61b
gaferguz <49653865+gaferguz@users.noreply.github.com>
2026-01-03 17:00:35 +0100

    Add files via upload
    Seleccion_transcritos_okaycull: codigo para sacar los ids de los transcritos de interes despues de usar tr2aacds.pl de EvidenceGene previo a una cuantificación con kallisto y a usar TPM_Filtering.R
    
    TPM_Filtering.R: Script de filtrado mayor o igual a 1 TPM usado en dos ocasiones consecutivas:
    - Para sacar los transcritos >= 1TPM de cada transcriptoma de Trinity y Spades
    - Para sacar los transcritos >= 1TPM de la fusión de los sets okay y cull de EvidenceGene.

---
+++ Filtrado_TPM/Seleccion_transcritos_okaycull.R
@@ -0,0 +1,24 @@
+# Leemos los transcritos cull y okay como dos tablas separadas
+cull_meta <-  read.table("./cullset.tsv", header = FALSE, sep = "\t", )
+colnames(cull_meta) <- c("Public_mRNA_ID","originalID","PublicGeneID","AltNum",	
+                         "Class","AAqual","pIdAln","Notes","Oids")
+
+# Para los cull solo nos interesaria las isoformas no descartadas
+cull_meta <- cull_meta[!grepl("cullalt", cull_meta$Class),]
+
+okay_meta <-  read.table("./okayset.tsv", header = FALSE, sep = "\t", )
+colnames(okay_meta) <- c("Public_mRNA_ID","originalID","PublicGeneID","AltNum",	
+                         "Class","AAqual","pIdAln","Notes","Oids")
+
+# Reading Kallisto tsv tables for RNASpades and Trinity (Full sample TPM)
+okaycull <- read.csv2("./Kallisto_OkayCull/Kallisto_Merged/Abundance.tsv", sep = " ")
+
+# Dividing okayset and cullset into two different TPM dataframes
+cull <- okaycull[okaycull$target_id %in% cull_meta$Public_mRNA_ID,] # 16696
+okay <- okaycull[okaycull$target_id %in% okay_meta$Public_mRNA_ID,] # 52352
+
+# After filtering with TPM >= 5 for cullnoclass and full TPM >=0 okayset
+okaycullset <- c(okay_tx, cull_tx) 
+length(unique(gsub("t.*","",over1TPM_okaycull)))
+
+data.table::fwrite(list(okaycullset), file = "./Okay_cullset.txt")
---
+++ Filtrado_TPM/TPM_Filtering.R
@@ -0,0 +1,70 @@
+# Cargamos las librerias necesarias 
+library(dplyr)
+library(ggplot2)
+library(data.table)
+library(MatrixGenerics)
+library(matrixStats)
+library(ggpubr)
+
+
+##################################
+#           FUNCIONES            #
+##################################
+
+TPM_filtering <- function(filtroTPM, datos, factor, TPMs){
+  # Inicalizacion de un vector que conteanga los ids de los transcritos
+  Passed_tx <- c()
+  # Iteracion usando como subgrupo la columna "factor" de "datos"
+  for (group in unique(datos[,factor])) {
+    subdatos <- datos[grepl(group, datos[,factor]),]
+    submatrix <- TPMs[colnames(TPMs) %in% subdatos$sample]
+    threshold <- round(3/4*(ncol(submatrix)),0)
+    # Binarizacion de la matriz
+    # Valores >= 1 seran 1, valores menores seran 0, luego hacemos rowsums
+    # Iteramos columna a columna para estar seguros de la conversion numerica
+    submatrix <- as.matrix(submatrix)
+    class(submatrix) <- "numeric"
+    submatrix[submatrix < filtroTPM] <- 0
+    submatrix[submatrix >= filtroTPM] <- 1
+    # Despues de binarizar sumamos las filas y si ésta es >= theshold, el transcrito pasa el filtro
+    Sum <- rowSums(submatrix)
+    subdf_sum <- data.frame(TPMs$target_id, Sum)
+    subdf_sumf2 <- subdf_sum %>% filter(Sum >= threshold)
+    colnames(subdf_sumf2) <- c("target_id","Sum")
+    # Se almacenan los ids de los transcritos que pasan y se eliminan redundancias
+    Passed_tx <- c(Passed_tx, subdf_sumf2$target_id)
+    Passed_tx <- unique(Passed_tx)
+    # Informacion que aparece por pantalla
+    print(paste0("N de tx: ",length(Passed_tx), " - Grupo: ", group, "- Umbral 75: ", threshold))
+  }
+  return(Passed_tx)
+}
+
+
+########################
+#         MAIN         #
+########################
+
+datos <- read.csv2("../5.DEG/samples.txt", sep = "\t")
+
+factor <- c("condition")
+
+# Esta misma funcion se uso con matrices de abundancia TPM sacadas del script de kallisto para:
+
+# 1 : Primeros ensamblajes filtrados (para Trinity y Spades)
+
+# 2 : Filtrado de los transcritos de EvidenGene tr2aacds (okay + cull)
+
+# Ejemplo
+
+TPMs <- read.csv2("../5.DEG/Kallisto_Enriched/Kallisto_Merged/Abundance.tsv", sep = " ")
+# modificacion de las columnas para que sean iguales a los nombres de las muestras en los metadatos
+colnames(TPMs) <- gsub("\\.","-",
+                       gsub("tpm_","", 
+                            gsub("_S.*","", colnames(TPMs))))
+
+
+Transcritos_seleccionados <- TPM_filtering(1, datos, factor, TPMs)
+
+# Escribimos un archivo de texto con los transcritos seleccionados para filtrarlos con seqtk
+data.table::fwrite(list(Transcritos_seleccionados), file = "./Okay_over1TPM.txt")
