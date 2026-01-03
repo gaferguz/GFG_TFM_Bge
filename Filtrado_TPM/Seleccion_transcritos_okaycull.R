f6e844b8cf4e02d1492759a201d7f8367c6bc38d
gaferguz <49653865+gaferguz@users.noreply.github.com>
2026-01-03 17:37:34 +0100

    Update Seleccion_transcritos_okaycull.R

--- Filtrado_TPM/Seleccion_transcritos_okaycull.R
+++ Filtrado_TPM/Seleccion_transcritos_okaycull.R
@@ -10,11 +10,11 @@ okay_meta <-  read.table("./okayset.tsv", header = FALSE, sep = "\t", )
 colnames(okay_meta) <- c("Public_mRNA_ID","originalID","PublicGeneID","AltNum",	
                          "Class","AAqual","pIdAln","Notes","Oids")
 
-# Dividing okayset and cullset into two different TPM dataframes
-cull <- okaycull[okaycull$target_id %in% cull_meta$Public_mRNA_ID,] # 16696
-okay <- okaycull[okaycull$target_id %in% okay_meta$Public_mRNA_ID,] # 52352
+# Extraemos los ids de los transcritos de interes
+cull_tx <- cull_meta$Public_mRNA_ID
+okay_tx <- okay_meta$Public_mRNA_ID
 
 okaycullset <- c(okay_tx, cull_tx) 
-length(unique(gsub("t.*","",over1TPM_okaycull)))
+length(unique(gsub("t.*","",over1TPM_okaycull))) # Ver los locus totales
 
 data.table::fwrite(list(okaycullset), file = "./Okay_cullset.txt")
